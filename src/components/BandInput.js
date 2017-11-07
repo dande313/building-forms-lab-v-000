d224aab8b7ad259c4c889bdcff6afc0df8315747
Allen Anderson <dande313@gmail.com>
2017-11-07 16:45:34 +0000

    automatically backed up by learn

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,6 +1,32 @@
 import React, { Component } from 'react';
 
 class BandInput extends Component {
+  
+    constructor(props) {
+    super(props);
+
+    this.state = {
+      text: ''
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_BAND',
+      band: this.state.text,
+    });
+    this.setState({
+      text: '',
+    });
+  };
+  
   render() {
 
     const bands = this.props.store.getState().bands.map((band, index) => {
