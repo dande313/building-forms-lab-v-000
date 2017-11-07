d712a8cd556f501f3df07170773c2bf2c3c41ec3
Allen Anderson <dande313@gmail.com>
2017-11-07 16:47:09 +0000

    automatically backed up by learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,44 +2,17 @@ import React, { Component } from 'react';
 
 class Bands extends Component {
 
-  constructor(props) {
-  super(props);
-
-  this.state = {
-    text: ''
-  };
-}
-
-handleChange(event) {
-  this.setState({
-    text: event.target.value,
-  });
-};
-
-handleSubmit(event) {
-  event.preventDefault();
-  this.props.store.dispatch({
-    type: 'ADD_BAND',
-    band: this.state.text,
-  });
-  this.setState({
-    text: '',
-  });
-};
-
-
-  render(){
+render() {
+    const bands = this.props.store.getState().bands.map((band, index) => {
+      return <li key={index}>{band}</li>
+    })
     return (
-      <form onSubmit={(event) => this.handleSubmit(event)}>
-        <p>
-          <label><strong>Band </strong></label>
-            <input type="text" value={this.state.text}
-            onChange={(event) => this.handleChange(event)} />
-        </p>
-        <input type="submit" />
-      </form>
+      <ul>
+        {bands}
+      </ul>
     );
   }
 };
+
 
 export default Bands;
